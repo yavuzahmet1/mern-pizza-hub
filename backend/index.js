8cc61d00009b23503414d6b57417290977e2a9ab
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-29 18:34:34 +0300

    ✨ improve(structure): Move config & middleware to src/ for cleaner architecture

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import dbConnection from "./config/dbConnection.js";
+import dbConnection from "./src/config/dbConnection.js";
 
 dotenv.config();
 const app = express();
