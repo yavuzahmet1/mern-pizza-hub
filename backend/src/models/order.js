bc922e5d851b4037e9b4ec3e1cdd451a9b160d61
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-05-01 02:35:02 +0300

    feat(model): 🍕 add Order schema for pizza ordering system
    - Defines Order model with userId, pizzaId, size, quantity, price, and totalPrice.
    - Adds enum validation for size and price calculation via pre-save hook.

---
+++ backend/src/models/order.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    pizzaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pizza",
+      required: true,
+    },
+    size: {
+      type: String,
+      trim: true,
+      required: true,
+      enum: ["Small", "Medium", "Large", "XLarge"],
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+      min:1
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+        type: Number,
+      },
+    },
+  },
+  { collection: "Order", timestamps: true }
+);
+
+OrderSchema.pre("save", function (next) {
+  this.totalPrice = this.quantity * this.price;
+  next();
+});
+const Order = mongoose.model("Order", OrderSchema);
+export default Order;
